93cb197ea9c81b66d65a407bb4b2c9b08ba79aed
hisheikhlab <73468743+hisheikhlab@users.noreply.github.com>
2023-09-14 21:30:45 +0500

    changes meta tag

--- django/pro1/app/views.py
+++ django/pro1/app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+import time
 
 import os
 from django.conf import settings
